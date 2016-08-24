3dc74692b9d4818c06aa68f27d07d3ca40826e20
ipulipin <ipulmi.d3@gmail.com>
2016-08-24 09:33:39 +0700

    revisi 24-agustus-2016:           + loading in imageactivity

--- app/src/main/java/com/floo/lenteramandiri/data/ImageActivity.java
+++ app/src/main/java/com/floo/lenteramandiri/data/ImageActivity.java
@@ -1,8 +1,11 @@
 package com.floo.lenteramandiri.data;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.PointF;
 import android.graphics.RectF;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -14,11 +17,18 @@ import com.floo.lenteramandiri.utils.MyLenteraMandiri;
 import com.floo.lenteramandiri.utils.TouchImageView;
 import com.squareup.picasso.Picasso;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import dmax.dialog.SpotsDialog;
+
 /**
  * Created by Floo on 4/21/2016.
  */
 public class ImageActivity extends AppCompatActivity implements ConnectivityReceiver.ConnectivityReceiverListener{
     TouchImageView img;
+    private SpotsDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +38,13 @@ public class ImageActivity extends AppCompatActivity implements ConnectivityRece
 
         img = (TouchImageView)findViewById(R.id.img_img);
 
-        Picasso.with(getApplicationContext())
-                .load("http://sandbox.floostudio.com/lenteramandiri/portofolio_csv/tfd_"+gambar+".jpg")
-                .into(img);
+        String url = "http://sandbox.floostudio.com/lenteramandiri/portofolio_csv/tfd_"+gambar+".jpg";
+
+        new ProfilLoadTask(url, img).execute();
+
+        //Picasso.with(getApplicationContext())
+        //        .load("http://sandbox.floostudio.com/lenteramandiri/portofolio_csv/tfd_"+gambar+".jpg")
+        //        .into(img);
     }
 
     @Override
@@ -43,5 +57,63 @@ public class ImageActivity extends AppCompatActivity implements ConnectivityRece
         super.onResume();
         MyLenteraMandiri.getInstance().setConnectivityListener(this);
         DataManager.checkConnection(getApplicationContext());
+    }
+
+    public class ProfilLoadTask extends AsyncTask<Void, Void, Bitmap> {
+
+        private String url;
+        private TouchImageView imageView;
+
+        public ProfilLoadTask(String url, TouchImageView imageView) {
+            this.url = url;
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new SpotsDialog(ImageActivity.this, R.style.CustomProgress);
+            pDialog.setMessage("Please wait...!!!");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            try {
+
+                URL urlConnection = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) urlConnection
+                        .openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+
+                Bitmap myBitmap = null;
+                if (connection.getResponseCode()==200){
+
+                    InputStream input = connection.getInputStream();
+                    myBitmap = BitmapFactory.decodeStream(input);
+                }else {
+                    myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.profile);
+                }
+
+
+                return myBitmap;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+
+            imageView.setImageBitmap(result);
+        }
     }
 }
